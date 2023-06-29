e88a77c03afe04c4bd7dec485b449b4c1c529274
CesarGomezPeloso <cesargomezpeloso@outlook.com>
2023-06-29 13:48:26 -0300

    2doPushMac

--- review_project/api/src/app.js
+++ review_project/api/src/app.js
@@ -3,15 +3,7 @@ const morgan = require("morgan");
 const mainRouter = require ("./routes");
 
 const app = express();
-
 app.use(morgan("dev"));
-
 app.use(mainRouter);
-
-// app.use((req, res, next) => {
-//   console.log("Hola estoy pasando por un middleware");
-//   next();
-// });
-
 
 module.exports = app;
