f74c5325d6f62d8d9fa8beb21dd318746794af3a
CesarGomezPeloso <cesargomezpeloso@outlook.com>
2023-08-04 20:06:16 -0300

    Clase 4 50min

---
+++ review_project/client/src/components/Card/Card.jsx
@@ -0,0 +1,12 @@
+const Card = () => {
+  return (
+    <div>
+      <p>
+        Este componente debe mostrar... la info de cada Usuario mapeado, pero
+        ademas, darnos un link para ir al detalle del usuario en cuestion
+      </p>
+    </div>
+  );
+};
+
+export default Card;
---
+++ review_project/client/src/components/CardsContainer/CardsContainer.jsx
@@ -0,0 +1,17 @@
+const CardsContainer = () => {
+
+    return (
+        <div>
+          <p>
+            Este Componente debe tomar una array de usuarios, 
+            y por cada usuario, renderizar un componente
+            CARD  
+         </p>
+        </div>
+      );
+    };
+
+
+
+
+export default CardsContainer
