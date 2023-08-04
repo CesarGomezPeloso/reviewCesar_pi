d5510208e9d2d027f99f043642e75d52e6a7ed2b
CesarGomezPeloso <cesargomezpeloso@outlook.com>
2023-08-04 10:54:47 -0300

    Clase 4 44:37

--- review_project/client/src/App.js
+++ review_project/client/src/App.js
@@ -1,14 +1,18 @@
+import { useLocation } from "react-router-dom/cjs/react-router-dom.min";
+import NavBar from "./components/NavBar/NabBar";
 import { Home, Landing, Detail, Form } from "./views";
 
 import { Route } from "react-router-dom";
 
 function App() {
+  const location = useLocation();
   return (
     <div className="App">
+      {location.pathname !== "/" && <NavBar />}
       <Route exact path="/" component={Landing} />
+      <Route path="/Home" render={() => <Home />} />
       <Route exact path="/Detail" component={Detail} />
       <Route exact path="/create" component={Form} />
-      <Route path="/Home" render={() => <Home />} />
     </div>
   );
 }
