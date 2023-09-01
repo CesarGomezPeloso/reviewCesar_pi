d4ea9b60b45e15f3b1471c9c37f949223b157a29
CesarGomezPeloso <cesargomezpeloso@outlook.com>
2023-09-01 10:34:32 -0300

    Clase 5-1

--- review_project/client/src/views/Home/Home.jsx
+++ review_project/client/src/views/Home/Home.jsx
@@ -1,10 +1,21 @@
-import CardsContainer from "../../components/CardsContainer/CardsContainer"
+import CardsContainer from "../../components/CardsContainer/CardsContainer";
+import { useEffect } from "react";
+import { useDispatch } from "react-redux";
+import { getUsers } from "../../redux/actions"; 
+
 
 const Home = () => {
+  //Cuando se monta, que haga el Dispatch
+  // useEffect()   -    useDispatch()
+  const dispatch = useDispatch();
+  useEffect (()=>{
+      dispatch (getUsers());  
+  },[])
+
   return (
     <>
       <h1>Esta es la Vista de Home</h1>
-      <CardsContainer/>
+      <CardsContainer />
     </>
   );
 };
